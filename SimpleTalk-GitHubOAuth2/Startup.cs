0316175b6ea9b66bc17ed1432b09324217149316
Stephen Yoon <stephen.yoon@tecsys.com>
2020-09-18 16:38:49 -0400

    Updated Startup.cs to implement OAuth requirements

--- SimpleTalk-GitHubOAuth2/Startup.cs
+++ SimpleTalk-GitHubOAuth2/Startup.cs
@@ -1,12 +1,17 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Security.Claims;
+using System.Text.Json;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OAuth;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Newtonsoft.Json.Linq;
 
 namespace SimpleTalk_GitHubOAuth2
 {
@@ -23,6 +28,48 @@ namespace SimpleTalk_GitHubOAuth2
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = "GitHub";
+            })
+               .AddCookie()
+               .AddOAuth("GitHub", options =>
+               {
+                   // Access information set up in GitHub's Developer settings page
+                   options.ClientId = Configuration["GitHub:ClientId"];
+                   options.ClientSecret = Configuration["GitHub:ClientSecret"];
+                   options.CallbackPath = new PathString("/github-oauth");
+
+                   // Endpoints where the provider will search for the auth, token and user information
+                   options.AuthorizationEndpoint = "https://github.com/login/oauth/authorize";
+                   options.TokenEndpoint = "https://github.com/login/oauth/access_token";
+                   options.UserInformationEndpoint = "https://api.github.com/user";
+
+                   // Guarantees that the tokens will be stored after each request finishes
+                   options.SaveTokens = true;
+
+                   // Claim actions to understand which information must be injected
+                   options.ClaimActions.MapJsonKey(ClaimTypes.NameIdentifier, "id");
+                   options.ClaimActions.MapJsonKey(ClaimTypes.Name, "name");
+                   options.ClaimActions.MapJsonKey("urn:github:login", "login");
+                   options.ClaimActions.MapJsonKey("urn:github:url", "html_url");
+                   options.ClaimActions.MapJsonKey("urn:github:avatar", "avatar_url");
+                   options.Events = new OAuthEvents
+                   {
+                       OnCreatingTicket = async context =>
+                       {
+                           var request = new HttpRequestMessage(HttpMethod.Get, context.Options.UserInformationEndpoint);
+                           request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                           request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", context.AccessToken);
+                           var response = await context.Backchannel.SendAsync(request, HttpCompletionOption.ResponseHeadersRead, context.HttpContext.RequestAborted);
+                           response.EnsureSuccessStatusCode();
+                           var json = JsonDocument.Parse(await response.Content.ReadAsStringAsync());
+                           context.RunClaimActions(json.RootElement);
+                       }
+                   };
+               });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -41,11 +88,15 @@ namespace SimpleTalk_GitHubOAuth2
 
             app.UseRouting();
 
-            app.UseAuthorization();
+            app.UseAuthentication();
 
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapControllers();
                 endpoints.MapRazorPages();
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller}/{action=Index}/{id?}");
             });
         }
     }
