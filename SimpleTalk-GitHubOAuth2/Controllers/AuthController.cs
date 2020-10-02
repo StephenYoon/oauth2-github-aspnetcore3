7ac4e0bb558ade73e0ea74f38c328b46a10db3d4
Stephen Yoon <stephen.yoon@tecsys.com>
2020-10-02 15:16:49 -0400

    Added AuthController

---
+++ SimpleTalk-GitHubOAuth2/Controllers/AuthController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SimpleTalk_GitHubOAuth2.Controllers
+{
+    [Route("[controller]/[action]")]
+    public class AuthController : Controller
+    {
+        [HttpGet]
+        public IActionResult Login(string returnUrl = "/")
+        {
+            return Challenge(new AuthenticationProperties() { RedirectUri = returnUrl });
+        }
+    }
+}
